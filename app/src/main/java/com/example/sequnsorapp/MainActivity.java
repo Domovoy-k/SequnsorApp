5e91437d53b44a1bf7a20b2e55a23e9c6a556da7
Domovoy <afss_rf@list.ru>
2020-02-18 20:40:40 +1000

    Initial commit

---
+++ app/src/main/java/com/example/sequnsorapp/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.sequnsorapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.usage.UsageEvents;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+    public class MainActivity extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+        }
+
+        Button button01 = findViewById(R.id.button01);
+
+        
+
+
+    }
